1dc501c70d8b536f768377bc86960390493baa14
lee-janice <janicel375@gmail.com>
2023-08-06 16:50:26 -0400

    add questions

--- components/flashcards.js
+++ components/flashcards.js
@@ -2,7 +2,6 @@ import React from "react";
 import Flashcard from "./flashcards/flashcard";
 
 const Flashcards = function ({ cards }) {
-    console.log(cards);
     return (
         <div className="flashcards">
             {cards.map((card, i) => {
---
+++ posts/lesson-1/components/automaton.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+const Automaton = function ({ cols, cells, answer }) {
+    const [selection, setSelection] = useState("");
+    return (
+        <div style={{ display: "inline-block", width: "100%" }}>
+            <div
+                style={{
+                    display: "grid",
+                    gridTemplateColumns: `repeat(${cols}, 50px)`,
+                    float: "left",
+                }}
+            >
+                {cells.map((cell) => {
+                    switch (cell) {
+                        case 0:
+                            return <div className="cell" />;
+                        case 1:
+                            return <div className="cell" style={{ backgroundColor: "black" }} />;
+                        case 2:
+                            return <div className="cell" style={{ backgroundColor: "red" }} />;
+                        default:
+                            return;
+                    }
+                })}
+            </div>
+            <div style={{ float: "left", marginLeft: "50px", cursor: "pointer" }} onClick={() => setSelection("1")}>
+                Alive
+                <div className="cell" style={{ backgroundColor: "black" }} />
+            </div>
+            <div style={{ float: "left", marginLeft: "50px", cursor: "pointer" }} onClick={() => setSelection("0")}>
+                Dead
+                <div className="cell" />
+            </div>
+            <br />
+            <div
+                style={{
+                    display: selection === "" ? "none" : "inline-block",
+                    marginLeft: "50px",
+                    // marginTop: "15px",
+                    padding: "10px",
+                    border: "1px solid black",
+                    borderRadius: "5px",
+                }}
+            >
+                {selection === answer
+                    ? "✅ Great job! The cell would be " + (answer === "0" ? "Dead." : "Alive.")
+                    : "❌ Nice try! The correct answer was " + (answer === "0" ? "Dead." : "Alive.")}
+            </div>
+        </div>
+    );
+};
+
+export default Automaton;
